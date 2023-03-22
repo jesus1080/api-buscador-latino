9e69d00da4c9dba5f177e2c14da5d6e2059b2e0b
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2023-03-21 23:12:24 -0500

    feat add Search model and migrations

--- search/models.py
+++ search/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Search(models.Model):
+    palabra = models.CharField(max_length=200)
+    resumen = models.TextField
+    contenido = models.TextField
+    
